c306cff29ed9e8773d3751aa66c2d76bc3b7db9f
Aleksenko Artem <a.o.aleksenko@student.khai.edu>
2022-11-12 17:50:26 +0200

    Hangman 4-rd stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,9 +1,6 @@
 package Hangman;
 
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class hangman {
     public static void main(String[] args) {
@@ -13,20 +10,27 @@ public class hangman {
 
         System.out.println("HANGMAN");
 
-        System.out.print("Guess the word: ");
-        String word = scanner.nextLine();
-
         var words = new ArrayList<String>();
         words.add("java");
         words.add("python");
         words.add("javascript");
         words.add("kotlin");
         words.trimToSize();
-//        System.out.println(words);
-//        System.out.println(words.size());
-        int n = random.nextInt(words.size());
-//        System.out.println(words.get(n));
-        if (word.equals(words.get(n))) {
+
+        String[] w = new String[]{};
+        String randomWord = words.get(random.nextInt(words.size()));
+        w = randomWord.split("");
+//        System.out.println(Arrays.toString(w));
+
+        System.out.print("Guess the word " + w[0]+w[1]);
+        for (int i = 2; i < w.length; i++){
+            System.out.print("-");
+        }
+
+        System.out.print(" : ");
+
+        String word = scanner.nextLine();
+        if (word.equals(randomWord)) {
             System.out.println("You win");
         } else {
             System.out.println("You loss");
