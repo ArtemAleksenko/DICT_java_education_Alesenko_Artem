abc28d75bd090294985c4c72e82b5d4d87ea596d
Aleksenko Artem <a.o.aleksenko@student.khai.edu>
2022-11-26 20:27:45 +0200

    Hangman 5-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,6 +1,7 @@
 package Hangman;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class hangman {
     public static void main(String[] args) {
@@ -9,32 +10,59 @@ public class hangman {
         Random random = new Random();
 
         System.out.println("HANGMAN");
-
+        // список слов
         var words = new ArrayList<String>();
         words.add("java");
         words.add("python");
         words.add("javascript");
         words.add("kotlin");
         words.trimToSize();
-
+        // слово делится на символы и засувуется в масив 1
         String[] w = new String[]{};
         String randomWord = words.get(random.nextInt(words.size()));
         w = randomWord.split("");
-//        System.out.println(Arrays.toString(w));
+        System.out.println(Arrays.toString(w));
 
-        System.out.print("Guess the word " + w[0]+w[1]);
-        for (int i = 2; i < w.length; i++){
-            System.out.print("-");
+        int times = 8;
+        // тут мы делаем пустой масив длинной с масив 1 и заполняем его прочерками
+        String[] unshown = new String[w.length];
+        for(int i = 0; i < unshown.length; i++){
+            unshown[i] = "-";
         }
+        while (times > 0){
+            AtomicBoolean check = new AtomicBoolean(false);
+            boolean checkEnter = false;
+            System.out.println(Arrays.toString(unshown));
+            String leter;
+            System.out.print("Input a letter: > ");
+            leter = scanner.nextLine();
+//            System.out.println(randomWord.indexOf(later));
 
-        System.out.print(" : ");
+            if(randomWord.contains(leter)) {
+                for (int i = 0; i < randomWord.length(); i++){
+                    if (randomWord.charAt(i) == leter.charAt(0) && unshown[i].equals("-")) {
+                        unshown[i] = leter;
+                        checkEnter = true;
+                    }
+                }
+                if (!checkEnter){
+                    times--;
+                }
+                Arrays.stream(unshown).forEach(item -> {
+                    if(item.equals("-")) check.set(true);
+                });
+                if(!check.get()){
+                    break;
+                }
+            }
+            else{
+                System.out.println("That letter doesn`t appear in the word");
+                times --;
+            }
 
-        String word = scanner.nextLine();
-        if (word.equals(randomWord)) {
-            System.out.println("You win");
-        } else {
-            System.out.println("You loss");
         }
+        System.out.println("Thanks for playing!\n" +
+                "We`ll see how well you did in the next stage");
 
     }
 }
