4ec61c67d3a2bae51b06dbffb98e2cde693c6bf6
Aleksenko Artem <a.o.aleksenko@student.khai.edu>
2022-12-17 21:24:16 +0200

    Hangman 7-8 stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -10,6 +10,7 @@ public class hangman {
         Random random = new Random();
 
         System.out.println("HANGMAN");
+
         // список слов
         var words = new ArrayList<String>();
         words.add("java");
@@ -17,56 +18,94 @@ public class hangman {
         words.add("javascript");
         words.add("kotlin");
         words.trimToSize();
-        // слово делится на символы и засувуется в масив 1
-        String[] w = new String[]{};
-        String randomWord = words.get(random.nextInt(words.size()));
-        w = randomWord.split("");
+
+        boolean work = true;
+
+        while (work) {
+            System.out.print("Type 'play'; to play the game, 'exit' to quit: ");
+            String mode = scanner.nextLine();
+            switch (mode) {
+                case "play": {
+                    // слово делится на символы и засувуется в масив 1
+                    String[] w = new String[]{};
+                    String randomWord = words.get(random.nextInt(words.size()));
+                    w = randomWord.split("");
 //        System.out.println(Arrays.toString(w));
 
-        int times = 8;
-        // тут мы делаем пустой масив длинной с масив 1 и заполняем его прочерками
-        String[] unshown = new String[w.length];
-        for (int i = 0; i < unshown.length; i++) {
-            unshown[i] = "-";
-        }
-        while (times > 0) {
-            AtomicBoolean check = new AtomicBoolean(false);
-            boolean checkEnter = false;
-            System.out.println(Arrays.toString(unshown));
-            String leter;
-            System.out.print("Input a letter: > ");
-            leter = scanner.nextLine();
-//            System.out.println(randomWord.indexOf(later));
-
-            if (randomWord.contains(leter)) {
-                for (int i = 0; i < randomWord.length(); i++) {
-                    if (randomWord.charAt(i) == leter.charAt(0) && unshown[i].equals("-")) {
-                        unshown[i] = leter;
-                        checkEnter = true;
+                    int times = 8;
+                    // тут мы делаем пустой масив длинной с масив 1 и заполняем его прочерками
+                    String[] unshown = new String[w.length];
+                    for (int i = 0; i < unshown.length; i++) {
+                        unshown[i] = "-";
                     }
-                }
-                if (!checkEnter) {
-                    System.out.println("No improvements");
-                    times--;
-                }
-                Arrays.stream(unshown).forEach(item -> {
-                    if (item.equals("-")) check.set(true);
-                });
-                if (!check.get()) {
-                    System.out.println(Arrays.toString(unshown));
-                    System.out.println("You guessed the word!\n" +
-                            "You survived!");
+                    ArrayList<String> secondMistace = new ArrayList<>();
+                    ArrayList<String> dontUsedLeter = new ArrayList<>();
+
+
+                    while (times > 0) {
+                        AtomicBoolean check = new AtomicBoolean(false);
+                        boolean checkEnter = false;
+                        boolean notCorrectAnsw = false;
+                        boolean highLeter = false;
+                        System.out.println(Arrays.toString(unshown));
+                        String leter;
+                        System.out.print("Input a letter: > ");
+                        leter = scanner.nextLine();
+//            System.out.println(randomWord.indexOf(later));
+                        if (leter.length() > 1) {
+                            System.out.println("You should input a single letter.");
+                        } else {
+                            if (randomWord.contains(leter)) {
+                                for (int i = 0; i < randomWord.length(); i++) {
+                                    if (randomWord.charAt(i) == leter.charAt(0) && unshown[i].equals("-")) {
+                                        unshown[i] = leter;
+                                        checkEnter = true;
+                                    }
+                                }
+
+                                if (!checkEnter) {
+                                    System.out.println("You`ve already guessed this letter.");
+                                    if (!secondMistace.contains(leter)) {
+                                        secondMistace.add(leter);
+                                    }
+                                }
+
+                                Arrays.stream(unshown).forEach(item -> {
+                                    if (item.equals("-")) check.set(true);
+                                });
+                                if (!check.get()) {
+                                    System.out.println(Arrays.toString(unshown));
+                                    System.out.println("You guessed the word!\n" +
+                                            "You survived!");
+                                    break;
+                                }
+                            } else if (!dontUsedLeter.contains(leter)) {
+                                if (randomWord.contains(leter.toLowerCase())) {
+                                    System.out.println("Please enter a lowercase English letter.");
+                                } else {
+                                    dontUsedLeter.add(leter);
+                                    System.out.println("Not true letter");
+                                }
+                            } else if (dontUsedLeter.contains(leter)) {
+                                System.out.println("You`ve already used this letter.");
+                            }
+                        }
+                    }
+                    if (times == 0) {
+                        System.out.println(Arrays.toString(unshown));
+                        System.out.println("You loss!");
+                    }
                     break;
                 }
-            } else {
-                System.out.println("That letter doesn`t appear in the word");
-                times--;
+                case "exit":
+                {
+                    work = false;
+                    break;
+                }
+                default:
+                    System.out.println("Incorrect input");
             }
 
-        }
-        if (times == 0) {
-            System.out.println(Arrays.toString(unshown));
-            System.out.println("You loss!");
         }
     }
 }
